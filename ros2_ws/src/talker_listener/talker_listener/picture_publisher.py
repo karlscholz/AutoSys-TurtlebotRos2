572909d5a4bd8077e2f839b0ad28fe2d6ef1e12e
miriam <mgeiger@hm.edu>
2022-12-08 10:56:10 +0000

    changed message type to CompressedImgage in picture_publisher.py

--- ros2_ws/src/talker_listener/talker_listener/picture_publisher.py
+++ ros2_ws/src/talker_listener/talker_listener/picture_publisher.py
@@ -3,10 +3,11 @@ from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 from std_msgs.msg import String
 from cv_bridge import CvBridge
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 import cv2 as cv
 import numpy as np
 import threading
+from time import sleep
 
 class CameraBufferFlusherThread(threading.Thread):
     def __init__(self, camera, name='OcvBufferFlusher'):
@@ -20,14 +21,14 @@ class CameraBufferFlusherThread(threading.Thread):
             ret, self.last_frame = self.camera.read()
 
 class MinimalPublisher(Node):
-    qosProfile = QoSProfile(reliability=QoSReliabilityPolicy.BEST_EFFORT,history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,depth=2)
+    qosProfile = QoSProfile(reliability=QoSReliabilityPolicy.BEST_EFFORT,history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,depth=1)
     iCounter = 0
     cap = cv.VideoCapture(0)
     cam_cleaner = CameraBufferFlusherThread(cap)
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(Image, 'imagePi', self.qosProfile)
-        timer_period = .2  # seconds
+        self.publisher = self.create_publisher(CompressedImage, 'imagePi', self.qosProfile)
+        timer_period = 0.2  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
         self.im_list = []       
@@ -45,8 +46,8 @@ class MinimalPublisher(Node):
         #print(success)
         cv.putText(cv_image, f'{self.iCounter}',(50,50), cv.FONT_HERSHEY_TRIPLEX, 1, (255,0,0), thickness=2)
         print(self.iCounter)
-        self.publisher_.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-        cv.imwrite("img_last_send.jpg",cv_image)
+        self.publisher.publish(self.bridge.cv2_to_compressed_imgmsg(cv_image))
+        #cv.imwrite("img_last_send.jpg",cv_image)
         self.get_logger().info('Publishing an image')
         self.iCounter += 1
         cv.waitKey(1)
